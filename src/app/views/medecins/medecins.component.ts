cc20a1f26bf407cded7e4eb87d12e58f958f4e66
yramzi <yassine.ramzi@cegedim.com>
2022-05-24 13:51:13 +0100

    Initialiser l'utilisation du infinite scroll

--- src/app/views/medecins/medecins.component.ts
+++ src/app/views/medecins/medecins.component.ts
@@ -23,11 +23,59 @@ export class MedecinsComponent extends RechercheAbsractComponent<Medecin>{
     }
   );
 
+  throttle = 300;
+  scrollDistance = 1;
+  scrollUpDistance = 2;
+  sum = 100;
+  direction = "";
+  array = [];
+  
+
   constructor(
     protected formBuilder: FormBuilder,
     private medecinService: MedecinService
   ) {
     super(formBuilder);
+    this.appendItems(0, this.sum);
+  }
+
+
+  addItems(startIndex, endIndex, _method) {
+    for (let i = 0; i < this.sum; ++i) {
+      this.array[_method]([i, " ", this.generateWord()].join("")); 
+    }
+  }
+
+  generateWord() {
+    return "DM21"+this.sum;
+  }
+
+  appendItems(startIndex, endIndex) {
+    this.addItems(startIndex, endIndex, "push");
+  }
+
+  prependItems(startIndex, endIndex) {
+    this.addItems(startIndex, endIndex, "unshift");
+  }
+
+  onScrollDown(ev) {
+    console.log("scrolled down!!", ev);
+
+    // add another 20 items
+    const start = this.sum;
+    this.sum += 20;
+    this.appendItems(start, this.sum);
+
+    this.direction = "down";
+  }
+
+  onUp(ev) {
+    console.log("scrolled up!", ev);
+    const start = this.sum;
+    this.sum += 20;
+    this.prependItems(start, this.sum);
+
+    this.direction = "up";
   }
 
   public rechercherMedecins(): void {
