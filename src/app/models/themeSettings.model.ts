ab3ca8a7bda5cb7360984fb65f059e2dadb805ff
yramzi <yassine.ramzi@cegedim.com>
2022-06-11 19:43:35 +0100

    Conversion des images en Base 64

--- src/app/models/themeSettings.model.ts
+++ src/app/models/themeSettings.model.ts
@@ -4,10 +4,14 @@ export class ThemeSettings {
     public couleurTheme : string;
     public couleurMenuSelectione : string;
     public couleurPolice : string;
+    public favIconImageBase64: string;
+    public logoImageBase64: string;
 
-    public constructor(formGroup: FormGroup){
+    public constructor(formGroup: FormGroup, favIconImageBase64: string, logoImageBase64: string){
         this.couleurTheme = formGroup.get(['couleurTheme'])!.value;
         this.couleurMenuSelectione = formGroup.get(['couleurMenuSelectione'])!.value;
         this.couleurPolice = formGroup.get(['couleurPolice'])!.value;
+        this.favIconImageBase64 = favIconImageBase64;
+        this.logoImageBase64 = logoImageBase64;
     }
 }
--- src/app/views/settings/settings.component.ts
+++ src/app/views/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ThemeSettings } from '../../models/themeSettings.model';
 
@@ -7,14 +7,27 @@ import { ThemeSettings } from '../../models/themeSettings.model';
   templateUrl: './settings.component.html'
 })
 export class SettingsComponent {
+  
+  @ViewChild('labelImportFavicon')
+  public labelImportFavicon: ElementRef;
+
+  
+  @ViewChild('labelImportLogo')
+  public labelImportLogo: ElementRef;
 
   public settingsForm: FormGroup = this.formBuilder.group(
     {
       couleurTheme : new FormControl(null),
       couleurMenuSelectione : new FormControl(null),
-      couleurPolice : new FormControl(null)
+      couleurPolice : new FormControl(null),
+      logo: new FormControl(null),
+      favicon : new FormControl(null)
     }
   );
+
+  private favIconImageBase64: string;
+
+  private logoImageBase64: string;
 
   private themeWrapper = document.querySelector('body');
 
@@ -24,7 +37,21 @@ export class SettingsComponent {
   }
 
   public saveConfigurationPersonalisee(): void {
-    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm);
+    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm, this.favIconImageBase64, this.logoImageBase64 );
+    if(themeSettings.couleurTheme) {
+      this.themeWrapper!.style.setProperty('--themeColor', themeSettings.couleurTheme);
+    }
+    if(themeSettings.couleurMenuSelectione) {
+      this.themeWrapper!.style.setProperty('--selectedElementColor', themeSettings.couleurMenuSelectione);
+    }
+    if(themeSettings.couleurPolice) {
+      this.themeWrapper!.style.setProperty('--policeColor', themeSettings.couleurPolice);
+    }
+    console.log(themeSettings);
+  }
+
+  public onThemeChange(): void {
+    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm, this.favIconImageBase64, this.logoImageBase64 );
     if(themeSettings.couleurTheme) {
       this.themeWrapper!.style.setProperty('--themeColor', themeSettings.couleurTheme);
     }
@@ -36,24 +63,33 @@ export class SettingsComponent {
     }
   }
 
-  public onCouleurThemeChange(): void {
-    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm);
-    if(themeSettings.couleurTheme) {
-      this.themeWrapper!.style.setProperty('--themeColor', themeSettings.couleurTheme);
+  public favIconUpload(event:any): void {
+    var file = event.target.files.length;
+    for(let i=0;i<file;i++)
+    {
+       var reader = new FileReader();
+       reader.onload = (event:any) => 
+       {
+           this.favIconImageBase64 = event.target.result;
+           console.log(this.favIconImageBase64);
+       };
+       reader.readAsDataURL(event.target.files[i]);
+       this.labelImportFavicon.nativeElement.innerText = event.target.files[i].name;
     }
   }
 
-  public onCouleurMenuSelectioneChange(): void {
-    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm);
-    if(themeSettings.couleurMenuSelectione) {
-      this.themeWrapper!.style.setProperty('--selectedElementColor', themeSettings.couleurMenuSelectione);
-    }
-  }
-
-  public onCouleurPoliceChange(): void {
-    const themeSettings: ThemeSettings = new ThemeSettings(this.settingsForm);
-    if(themeSettings.couleurPolice) {
-      this.themeWrapper!.style.setProperty('--policeColor', themeSettings.couleurPolice);
+  public logoUpload(event:any): void {
+    var file = event.target.files.length;
+    for(let i=0;i<file;i++)
+    {
+       var reader = new FileReader();
+       reader.onload = (event:any) => 
+       {
+           this.logoImageBase64 = event.target.result;
+           console.log(this.logoImageBase64);
+       };
+       reader.readAsDataURL(event.target.files[i]);
+       this.labelImportLogo.nativeElement.innerText = event.target.files[i].name;
     }
   }
 
